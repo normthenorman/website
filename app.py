90389051fee7501f6133a4d2264f992b9138bbc4
norm <itsnormthenorman@proton.me>
2026-07-11 16:41:01 +0300

    save

--- app.py
+++ app.py
@@ -59,6 +59,9 @@ def logout():
 def index():
     return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 @app.route('/blog')
 def blog():
